c7edb93285b1c5a38f37ef51f7f88f008aa55507
sDEV-eloper <thisissurajdev@gmail.com>
2023-09-06 00:21:57 +0530

    completed till skills section

--- components/Body.jsx
+++ components/Body.jsx
@@ -1,23 +1,23 @@
-import React from 'react'
-import Home from './Home'
-import NavBar from './NavBar'
-import About from './About'
-import Skills from './Skills'
-import Projects from './Projects'
-import Contact from './Contact'
-import Footer from './Footer'
+import React from "react";
+import Home from "./Home";
+import NavBar from "./NavBar";
+import About from "./About";
+import Skills from "./Skills";
+import Projects from "./Projects";
+import Contact from "./Contact";
+import Footer from "./Footer";
 
 const Body = () => {
   return (
     <>
-        <Home/>
-        <About/>
-        <Skills/>
-        <Projects/>
-        <Contact/>
-        <Footer/>
+      <Home />
+      <About />
+      <Skills />
+      <Projects />
+      <Contact />
+      <Footer />
     </>
-  )
-}
+  );
+};
 
-export default Body
+export default Body;
--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -1,13 +1,11 @@
-"use client"
-import Link from 'next/link';
-import { useState } from 'react';
-import { AiOutlineClose } from 'react-icons/ai';
-import { RxHamburgerMenu} from 'react-icons/rx';
-import Logo from "@/public/images/slogo_circle.png"
-import ProfileImg from "@/public/images/myGif.gif"
-import Image from 'next/image';
-
-
+"use client";
+import Link from "next/link";
+import { useState } from "react";
+import { AiOutlineClose } from "react-icons/ai";
+import { RxHamburgerMenu } from "react-icons/rx";
+import Logo from "@/public/images/slogo_circle.png";
+import ProfileImg from "@/public/images/myGif.gif";
+import Image from "next/image";
 
 function NavBar() {
   const [navbar, setNavbar] = useState(false);
@@ -15,67 +13,71 @@ function NavBar() {
     <div>
       <nav className="w-full bg-white  fixed top-0 left-0 right-0 z-50">
         <div className="justify-between  px-4 mx-auto lg:max-w-7xl md:items-center md:flex md:px-8">
-            <div className="flex  items-center justify-between py-2 md:py-2 md:block">
-              <Link href="/">
-                <div className='flex items-center justify-center gap-4'>
-                <Image src={Logo} alt='S' className='w-12'/>
+          <div className="flex  items-center justify-between py-2 md:py-2 md:block">
+            <Link href="/">
+              <div className="flex items-center justify-center gap-4">
+                <Image src={Logo} alt="S" className="w-12" />
                 <h2 className="text-2xl text-cyan-800 font-bold ">PORTFOLIO</h2>
-           
-                </div>
-                </Link>
-              <div className="md:hidden">
-                <button
-                  className="p-2 text-gray-700 rounded-md outline-none focus:border-gray-400 focus:border"
-                  onClick={() => setNavbar(!navbar)}
-                >
-                  {navbar ? (
-                      <span className='text-black text-2xl'>
-                        <AiOutlineClose/>
-                </span>
-                  ) : (
-                    <span className='text-black text-2xl'>
-                        <RxHamburgerMenu />
-                    </span>
-                  )}
-                </button>
               </div>
+            </Link>
+            <div className="md:hidden">
+              <button
+                className="p-2 text-gray-700 rounded-md outline-none focus:border-gray-400 focus:border"
+                onClick={() => setNavbar(!navbar)}
+              >
+                {navbar ? (
+                  <span className="text-black text-2xl">
+                    <AiOutlineClose />
+                  </span>
+                ) : (
+                  <span className="text-black text-2xl">
+                    <RxHamburgerMenu />
+                  </span>
+                )}
+              </button>
             </div>
-            <div
-              className={`flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 ${
-                navbar ? 'p-12 md:p-0 block' : 'hidden'
-              }`}
-            >
-              <ul className="h-screen md:h-auto items-center justify-center md:flex ">
-                <li className=" text-md text-gray-700 py-2 md:px-6 text-center border-b-2 md:border-b-0  hover:bg-orange-500    md:hover:text-orange-500   md:hover:bg-transparent">
-                  <Link href="/" onClick={() => setNavbar(!navbar)}>
-                    Home
-                  </Link>
-                </li>
-                <li className=" text-md text-gray-700 py-2 md:px-6 text-center border-b-2 md:border-b-0  hover:bg-orange-500    md:hover:text-orange-500   md:hover:bg-transparent">
-                  <Link href="/#about" onClick={() => setNavbar(!navbar)}>
-                    About
-                  </Link>
-                </li>
-                <li className=" text-md text-gray-700 py-2 px-6 text-center  border-b-2 md:border-b-0  hover:bg-orange-500    md:hover:text-orange-500   md:hover:bg-transparent">
-                  <Link href="/#skills" onClick={() => setNavbar(!navbar)}>
-                    Skills
-                  </Link>
-                </li>
-                <li className=" text-md text-gray-700 py-2 px-6 text-center  border-b-2 md:border-b-0  hover:bg-orange-500    md:hover:text-orange-500   md:hover:bg-transparent">
-                  <Link href="/#projects" onClick={() => setNavbar(!navbar)}>
-                    Projects
-                  </Link>
-                </li>
-                <li className=" text-md text-gray-700 py-2 px-6 text-center  border-b-2 md:border-b-0  hover:bg-orange-500    md:hover:text-orange-500   md:hover:bg-transparent">
-                  <Link href="/#contact" onClick={() => setNavbar(!navbar)}>
-                    Contact
-                  </Link>
-                </li>
-              </ul>
-            </div>
-            <div className='hidden sm:hidden md:flex'>
-  <Image src={ProfileImg} alt="myprofile" width={40} className='rounded-full' />
-</div>
+          </div>
+          <div
+            className={`flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 ${
+              navbar ? "p-12 md:p-0 block" : "hidden"
+            }`}
+          >
+            <ul className="h-screen md:h-auto items-center justify-center md:flex ">
+              <li className=" text-md text-gray-700 py-2 md:px-6 text-center border-b-2 md:border-b-0  hover:bg-orange-500    md:hover:text-orange-500   md:hover:bg-transparent">
+                <Link href="/" onClick={() => setNavbar(!navbar)}>
+                  Home
+                </Link>
+              </li>
+              <li className=" text-md text-gray-700 py-2 md:px-6 text-center border-b-2 md:border-b-0  hover:bg-orange-500    md:hover:text-orange-500   md:hover:bg-transparent">
+                <Link href="/#about" onClick={() => setNavbar(!navbar)}>
+                  About
+                </Link>
+              </li>
+              <li className=" text-md text-gray-700 py-2 px-6 text-center  border-b-2 md:border-b-0  hover:bg-orange-500    md:hover:text-orange-500   md:hover:bg-transparent">
+                <Link href="/#skills" onClick={() => setNavbar(!navbar)}>
+                  Skills
+                </Link>
+              </li>
+              <li className=" text-md text-gray-700 py-2 px-6 text-center  border-b-2 md:border-b-0  hover:bg-orange-500    md:hover:text-orange-500   md:hover:bg-transparent">
+                <Link href="/#projects" onClick={() => setNavbar(!navbar)}>
+                  Projects
+                </Link>
+              </li>
+              <li className=" text-md text-gray-700 py-2 px-6 text-center  border-b-2 md:border-b-0  hover:bg-orange-500    md:hover:text-orange-500   md:hover:bg-transparent">
+                <Link href="/#contact" onClick={() => setNavbar(!navbar)}>
+                  Contact
+                </Link>
+              </li>
+            </ul>
+          </div>
+          <div className="hidden sm:hidden md:flex">
+            <Image
+              src={ProfileImg}
+              alt="myprofile"
+              width={40}
+              className="rounded-full"
+            />
+          </div>
         </div>
       </nav>
     </div>
