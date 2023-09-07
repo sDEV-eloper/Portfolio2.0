b004c6b55c88c051010343b4ca9db07bc529e4fa
sDEV-eloper <thisissurajdev@gmail.com>
2023-09-07 11:40:50 +0530

    fix statusindicator

--- components/StatusIndicator.jsx
+++ components/StatusIndicator.jsx
@@ -1,8 +1,10 @@
+
 "use client"
 import React, { useEffect, useState } from 'react';
 
 function StatusIndicator() {
-  const [online, setOnline] = useState(window.navigator.onLine);
+  const isClient = typeof window !== 'undefined';
+  const [online, setOnline] = useState(isClient ? window.navigator.onLine : true);
 
   useEffect(() => {
     const handleOnline = () => {
@@ -13,39 +15,36 @@ function StatusIndicator() {
       setOnline(false);
     };
 
-    window.addEventListener('online', handleOnline);
-    window.addEventListener('offline', handleOffline);
-
-    // Cleanup the event listeners when the component unmounts
-    return () => {
-      window.removeEventListener('online', handleOnline);
-      window.removeEventListener('offline', handleOffline);
-    };
-  }, []);
+    if (isClient) {
+      window.addEventListener('online', handleOnline);
+      window.addEventListener('offline', handleOffline);
+      
+      // Cleanup the event listeners when the component unmounts
+      return () => {
+        window.removeEventListener('online', handleOnline);
+        window.removeEventListener('offline', handleOffline);
+      };
+    }
+  }, [isClient]);
 
   const statusColor = online ? 'green' : 'red';
 
   return (
     <div className='flex items-center justify-center px-2 py-1  rounded-lg bg-white'>
-        <label className='font-medium'>Fun Feature 🤩</label> 
-        <div
-      style={{
-        width: '10px',
-        height: '10px',
-        borderRadius: '50%',
-        margin: '10px',
-        backgroundColor: statusColor,
-        color:statusColor
-      }}
-    >
-    </div>
-<div
- style={{
-    color:statusColor
-  }}
->
-{online?"You are Online":"You are Offline"}
-</div>
+      <label className='font-medium'>Fun Feature 🤩</label> 
+      <div
+        style={{
+          width: '10px',
+          height: '10px',
+          borderRadius: '50%',
+          margin: '10px',
+          backgroundColor: statusColor,
+          color: statusColor
+        }}
+      ></div>
+      <div style={{ color: statusColor }}>
+        {online ? "You are Online" : "You are Offline"}
+      </div>
     </div>
   );
 }
